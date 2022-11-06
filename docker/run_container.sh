dc48388ad8e4ffd1e8d244b5cc832369031eb5d4
TheOriginalTiger <vvp.vovarulit@gmail.com>
2022-11-06 21:18:17 +0700

    Add results to volumes so it is possible to see them

--- docker/run_container.sh
+++ docker/run_container.sh
@@ -1,5 +1,8 @@
 
 cname=th-diffusion
 docker_image=diffusion-talking-head
+result_path=~/nsu/diploma/results
 
-docker run --gpus all -it --rm --name $cname $docker_image
+docker run --gpus all -it --rm --name $cname \
+            -v $result_path:/results \
+            $docker_image
