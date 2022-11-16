6c02c89ba4be0f126eafbd974fbd4f6bc595a3be
TheOriginalTiger <vvp.vovarulit@gmail.com>
2022-11-16 17:17:56 +0700

    Add Embedding layers for timestamps

--- modules.py
+++ modules.py
@@ -26,6 +26,22 @@ class Down(nn.Module):
         return x
 
 
+class DownEmb(nn.Module):
+    def __init__(self, in_channels, out_channels, time_dim):
+        super(DownEmb, self).__init__()
+        self.down = Down(in_channels=in_channels, out_channels=out_channels)
+        self.emb = nn.Sequential(
+            nn.Linear(in_features=time_dim, out_features=out_channels),
+            nn.SiLU()
+        )
+
+    def forward(self, x, t):
+        x = self.down(x)
+        # transform [batch_size,channels] to -> [batch_size,chanels,feature_map_h,feature_map_w]
+        emb = self.emb(t)[:, :, None, None].repeat(1, 1, x.shape[-2], x.shape[-1])
+        return x + emb
+
+
 class Up(nn.Module):
     def __init__(self, in_channels, out_channels):
         super(Up, self).__init__()
@@ -54,3 +70,37 @@ class Up(nn.Module):
         x = self.conv1(x)
         x = self.conv2(x)
         return x
+
+
+class UpEmb(nn.Module):
+    def __init__(self, in_channels, out_channels, time_dim):
+        super(UpEmb, self).__init__()
+
+        self.up = Up(in_channels=in_channels, out_channels=out_channels)
+
+        self.emb = nn.Sequential(
+            nn.Linear(in_features=time_dim, out_features=out_channels),
+            nn.SELU()
+        )
+
+    def forward(self, x, residual, t):
+        x = self.up(x, residual)
+        emb = self.emb(t)[:, :, None, None].repeat(1, 1, x.shape[-2], x.shape[-1])
+        return x + emb
+
+
+class SinPositionalEncoding(nn.Module):
+    def __init__(self, time_dim: int, device: str):
+        super(SinPositionalEncoding, self).__init__()
+        self.time_dim = time_dim
+        self.device = device
+
+    def forward(self, t):
+        inv_freq = 1.0 / (
+            10000
+            ** (torch.arange(0, self.time_dim, 2, device=self.device).float() / self.time_dim)
+        )
+        pos_enc_a = torch.sin(t.repeat(1, self.time_dim // 2) * inv_freq)
+        pos_enc_b = torch.cos(t.repeat(1, self.time_dim // 2) * inv_freq)
+        pos_enc = torch.cat([pos_enc_a, pos_enc_b], dim=-1)
+        return pos_enc
