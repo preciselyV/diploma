42a80ecee8c18ac46cf6ef31622fd908a74fdcce
TheOriginalTiger <vvp.vovarulit@gmail.com>
2022-11-26 21:37:44 +0700

    Yeah... Turnes out we can't use proper 11.+ Cuda and locked at 10.2... 3 hours of struggling and I ported it to 10.2

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 cd ./core
 
-python3 diffusionModel.py dry-run --config_path /diffusion-th/configs/conf2.yml --name $1
+python3.8 diffusionModel.py dry-run --config_path /diffusion-th/configs/conf2.yml --name $1
